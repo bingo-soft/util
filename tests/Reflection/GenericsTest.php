e5617a89fb459eaf1be705e176f7189fa7d9118c
Viktor <vv_volkov@mail.ru>
2022-10-31 09:49:27 +0300

    fix: remove strict type checking

--- tests/Reflection/GenericsTest.php
+++ tests/Reflection/GenericsTest.php
@@ -7,18 +7,6 @@ use Util\Reflection\MetaObject;
 
 class GenericsTest extends TestCase
 {
-    public function testTypeErrorThrownWhenStrictTypeCheckingIsSet(): void
-    {
-        $child = new Child();
-        $meta = new MetaObject($child);
-        $meta->setStrictTypeChecking();
-
-        $meta->setValue('child', new Child());
-
-        $this->expectException(\TypeError::class);
-        $meta->setValue('child.id', 123);
-    }
-
     public function testTypeErrorNotThrownWhenStrictTypeCheckingNotSet(): void
     {
         $child = new Child();
